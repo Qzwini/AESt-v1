b668feeb793149ac337c89104c7ea6c653e1301c
Qzwini <qzwini@gmail.com>
2020-05-30 17:38:25 +0300

    AES

---
+++ cliaes.py
@@ -0,0 +1,13 @@
+def showCli():
+    print("""
+        \033[40m|||||||||||||||||||||||||||||||||||||||||||\033[0m
+
+                \033[42mCrypto AES script v0.1 \033[0m\n
+            |-> Coded by : \033[32m\033[01Qqzwini \033[0m\n
+            |-> Aes code : \033[32m\033[01eencryption/decryption  \033[0m\n
+            |-> securityII lab : \033[32m\033[01QAli qzwini \033[0m
+
+        \033[40m|||||||||||||||||||||||||||||||||||||||||||\033[0m
+    """)
+
+showCli()
